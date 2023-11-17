43fb99144a0382c2d26029d0f62910d58ed150ec
Kamrul Islam Pavel <117812369+00Pavel@users.noreply.github.com>
2023-11-18 00:05:46 +0600

    Update client_view.php

--- midtermProject/view/adminView/client_view.php
+++ midtermProject/view/adminView/client_view.php
@@ -6,15 +6,20 @@
 <body>
 
 <?php
-$jsonData = file_get_contents('../data/data.json');
+$jsonData = file_get_contents('../data/patientData.json');
 $phpdata = json_decode($jsonData, true);
-foreach ($phpdata as $data) 
+foreach ($phpdata as $data)
 {
     echo '<h1>User Information</h1>';
+    echo "<img src=".$data['Image']." />";
     echo '<p>Name: ' . $data['UserName'] . '</p>';
     echo '<p>Email: ' . $data['Email'] . '</p>';
-    echo '<p>Phone Number: ' . $data['Phone Number'] . '</p>'; 
+    echo '<p>Phone Number: ' . $data['Phone Number'] . '</p>';
     echo '<p>Gender: ' . $data['Gender'] . '</p>';
+    echo '<p>Age: ' . $data['age'] . '</p>';
+    echo '<p>Weight: ' . $data['Weight'] . '</p>';
+    echo '<p>Hight: ' . $data['Hight'] . '</p>';
+    echo '<p>Blood Group: ' . $data['Blood Group'] . '</p>';
     echo '<p>Address: ' . $data['Address'] . '</p>';
 }
 ?>
